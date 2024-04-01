65cf3a3e0f36759c3a8bdc48256e2ff4a9cedafc
rokas <rokas@rokas.com>
2024-04-01 17:13:01 +0300

    Add GestureHandlerRootView to TabOneScreen component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,34 +1,23 @@
 import {SafeAreaView, StyleSheet} from 'react-native';
-
-import { Text, View } from 'react-native';
 import {StatusBar} from "expo-status-bar";
 import CardList from "@/components/card-list";
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function TabOneScreen() {
   return (
-    // <GestureHandlerRootView style={{ flex: 1 }}>
+    <GestureHandlerRootView style={{ flex: 1 }}>
       <SafeAreaView style={styles.container}>
         <CardList />
 
         <StatusBar style="light" />
       </SafeAreaView>
-    // </GestureHandlerRootView>
+    </GestureHandlerRootView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+    backgroundColor: 'black',
+  }
 });
